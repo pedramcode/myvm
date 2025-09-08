833ac3488571e35918b2c3ffdf04ffb46442b1ef
pedramcode <dev.dehghanpour@gmail.com>
2025-09-09 01:43:51 +0330

    factorial example

--- examples/factorial.asm
+++ examples/factorial.asm
@@ -1,31 +1,26 @@
 [data]
 
-$num dw 5 6 7
+$num dw 0 1 2 3 4 5 6 7
 $title dw "Factorial result: " 13 10 0
 $msg dw "! = " 0
 $newline dw 10 13 0
 
 [text]
 
-push [$num]
-safecall .factorial
-pop r0
-
 push $title
 int 0 3
-push [$num]
-int 0 4
-push $msg
-int 0 3
-push r0
-int 0 4
-push $newline
-int 0 3
 
+move r0 7
+.for
+    push [$num + r0]
+    safecall .factorial
+    dec r0
+    jnz .for
 term
 
 .factorial
-    dup
+    dup 2
+    pop r5
     pop r1
     move r0 1 ; acc
     .loop
@@ -36,6 +31,13 @@ term
         dec r1
         push r1
         jg .loop
+    push r5
+    int 0 4
+    push $msg
+    int 0 3
     push r0
+    int 0 4
+    push $newline
+    int 0 3
 
     ret
