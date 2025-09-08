098e5f452ba9745e56a725761085f3991294b71e
pedramcode <dev.dehghanpour@gmail.com>
2025-09-08 19:11:46 +0330

    factorial

---
+++ examples/factorial.asm
@@ -0,0 +1,40 @@
+[data]
+
+$num dw 5
+$msg dw "! = " 0
+$newline dw 10 13 0
+
+[text]
+
+push [$num]
+call .factorial
+
+push [$num]
+int 0 4
+push $msg
+int 0 3
+push r0
+int 0 4
+push $newline
+int 0 3
+
+term
+
+.factorial
+    dup
+    pop r1
+    move r0 1 ; acc
+    .loop
+        push r0
+        mul
+        pop r0
+
+        dec r1
+        push r1
+        push 1
+        swap
+        sub
+        push r1
+        jg .loop
+
+    ret
